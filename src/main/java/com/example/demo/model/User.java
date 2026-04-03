a1dd1a5fdef673b0344462f27bb1ad227dcdfd05
Paradox-Adarsh <nikhilrv605@gmail.com>
2026-04-03 23:49:12 +0530

    changes user

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,18 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+
+@Data
 public class User {
 
     @Id
@@ -16,37 +25,10 @@ public class User {
     private String email;
 
     private String password;
+    
+    private String provider;      // GOOGLE
+    private String providerId;    // Google 'sub'
+    private String role;   
 
-    // getters & setters
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
- 
-    public void setEmail(String email) {
-        this.email = email;
-    }
- 
-    public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
